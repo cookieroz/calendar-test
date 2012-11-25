04fa76433e6b6320f368f44983248adb18c7388d
Xu Pan <cqpanxu@gmail.com>
2012-11-26 00:09:41 +0800

    remove jquery.rest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require jquery.ui.datepicker
-//= require jquery.rest
 //= require jquery-fileupload
 //= require fullcalendar
 //= require_tree .
